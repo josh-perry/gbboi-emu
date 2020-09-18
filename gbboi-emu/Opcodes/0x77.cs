cef1ce557eb848aa8ce65860879179f2deb8f49d
Josh Perry <joshperry@outlook.com>
2020-09-18 23:26:57 +0100

    Added 0x77

--- gbboi-emu/Opcodes/0x77.cs
+++ gbboi-emu/Opcodes/0x77.cs
@@ -3,13 +3,12 @@ using System;
 namespace gbboi_emu.Opcodes
 {
     /// <summary>
-    /// LD
-    ///
+    /// LD (HL),A
     /// </summary>
     [OneByteOpcode]
     public class _0x77 : IOpcode
     {
-        public string Mnemonic { get; set; } = "LD";
+        public string Mnemonic { get; set; } = "LD (HL),A";
 
         public ushort Length { get; set; } = 1;
 
@@ -19,7 +18,7 @@ namespace gbboi_emu.Opcodes
 
         public void Execute(Instruction instruction, ICpu cpu, IMemory memory)
         {
-            throw new NotImplementedException(Mnemonic);
+            memory.Bytes[cpu.Registers.HL.Value] = cpu.Registers.A.Value;
         }
     }
 }
